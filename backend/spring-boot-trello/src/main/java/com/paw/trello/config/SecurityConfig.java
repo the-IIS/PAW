79ad4f06376808e10f668ae16f13b4a8fdba0292
solid <solidsrv@gmail.com>
2020-05-06 01:11:03 +0200

    poprawa autentyfikacji w configu

--- backend/spring-boot-trello/src/main/java/com/paw/trello/config/SecurityConfig.java
+++ backend/spring-boot-trello/src/main/java/com/paw/trello/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.paw.trello.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -40,6 +42,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated();
 
         httpSecurity.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(HttpMethod.OPTIONS, "/**");
     }
 
     @Autowired
